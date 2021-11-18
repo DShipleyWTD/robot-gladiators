8b4ea6ac7f21be42ce5c146c6d4e2acb43deb150
Doug Shipley <dougshipley@Selos-MacBook-Air.local>
2021-11-18 10:32:08 -0800

    new enemy rounds added

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,8 +19,8 @@ var enemyAttack = 12;
 // "LOSE" - Player robot's health is zero or less
 
 var fight = function(enemyName) {
-  // Alert players that they are starting the round  
-  window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
 
   //Ask players if they want to fight or skip
   var promptFight = window.prompt("would you like to FIGHT or SKIP this battle?Enter 'FIGHT' or 'SKIP' to chooose.");
@@ -74,7 +74,12 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
    else {
     window.alert("You need to choose a valid option. Try again!");
   }
+}
 };
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i]
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
+ // Alert players that they are starting the round  
+ window.alert("Welcome to Robot Gladiators!");
